e0d255c3ac6c06f57010e03772c20e911e947d63
Jasqq <jaakkotasanko04@gmail.com>
2025-03-05 01:12:40 +0200

    Lisätty liikkuminen näppäimillä

--- src/Character.cpp
+++ src/Character.cpp
@@ -11,4 +11,12 @@ void Character::drawCharacter(){
 void Character::moveCharacter(float x, float y){
 	this->position.x += x;
 	this->position.y += y;
+	
+}
+
+void Character::updateCharacter(){
+    if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) moveCharacter(0, -1);
+    if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) moveCharacter(0, 1);
+    if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) moveCharacter(-1, 0);
+    if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) moveCharacter(1, 0);
 }
