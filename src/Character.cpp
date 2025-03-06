153ef22eae6f4ba09df23ff41a4ae205422aabfb
Jasqq <jaakkotasanko04@gmail.com>
2025-03-06 10:16:00 +0200

    Korjattu liikkumisen nopeus kun liikutaan viistoon

--- src/Character.cpp
+++ src/Character.cpp
@@ -15,8 +15,16 @@ void Character::moveCharacter(float x, float y){
 }
 
 void Character::updateCharacter(){
-    if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) moveCharacter(0, -1);
-    if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) moveCharacter(0, 1);
-    if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) moveCharacter(-1, 0);
-    if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) moveCharacter(1, 0);
+    float speed = 1.0f;
+    bool movingHorizontally = IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT);
+    bool movingVertically = IsKeyDown(KEY_W) || IsKeyDown(KEY_UP) || IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN);
+
+    if (movingHorizontally && movingVertically) {
+        speed *= 0.7f;
+    }
+
+    if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) moveCharacter(0, -speed);
+    if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) moveCharacter(0, speed);
+    if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) moveCharacter(-speed, 0);
+    if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) moveCharacter(speed, 0);
 }
